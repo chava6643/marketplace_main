197f94c5a96016c5aa6037d8a1905884d396108e
chava6643 <salvador27bravo@gmail.com>
2025-11-14 11:02:34 -0800

    se modifico el archivo views.py,urls.py,login.html,signup.html

--- store/views.py
+++ store/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import logout
-from .models import Item, Category
-from .forms import SignupForm
 
+from .models import Item, Category
+
+from .forms import SignupForm
 
 # Create your views here.
 def home(request):
     items = Item.objects.filter(is_sold=False)
     categories = Category.objects.all()
-
 
     context = {
         'items': items,
@@ -16,13 +16,12 @@ def home(request):
     }
     return render(request, 'store/home.html', context)
 
-
 def contact(request):
     context = {
         'msg': 'Quieres otros productos contactame!'
     }
-    return render(request, 'store/contact.html', context)
 
+    return render(request, 'store/contact.html', context)
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
@@ -32,14 +31,11 @@ def detail(request, pk):
         'related_items': related_items
     }
 
-
     return render(request, 'store/item.html', context)
-
 
 def register(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
-
 
         if form.is_valid():
             form.save()
@@ -47,11 +43,8 @@ def register(request):
     else:
         form = SignupForm()
 
-
     context = {
         'form': form
     }
 
-
     return render(request, 'store/signup.html', context)
-
